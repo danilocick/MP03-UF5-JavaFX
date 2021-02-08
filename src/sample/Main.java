6c4008e7320e8ffa47ec5280d0a9f7f92fdbfdb2
danilocick <dhernandez@elpuig.xeill.net>
2021-02-08 20:25:11 +0100

    inicio

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent bp = FXMLLoader.load(getClass().getResource("tablero.fxml"));
         primaryStage.setTitle("TIC TAC TOE");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(bp));
         primaryStage.show();
     }
 
